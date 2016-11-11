080c626a188e57cb2ace58b75b77417e2dfbd6e1
M A Wal-Ikram <meezaan@vesica.ws>
2016-11-11 07:34:29 +0000

    debugging code commented

--- aladhan.php
+++ aladhan.php
@@ -12,9 +12,10 @@
  * https://premium.wpmudev.org/blog/wordpress-plugin-development-guide/, https://codex.wordpress.org/Roles_and_Capabilities
  */
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// When debugging, uncomment the below lines.
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 require_once(realpath(__DIR__) . '/vendor/autoload.php');
 require_once(realpath(__DIR__) . '/classes/Render.php');
